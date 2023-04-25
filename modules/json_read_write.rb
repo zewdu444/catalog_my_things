509d3ed0fada953fbf34383d9a4d0706af6da722
Mohi <MOHISADEGHI302@GMAIL.com>
2023-04-25 20:33:14 +0330

    Add json write|read files

---
+++ modules/json_read_write.rb
@@ -0,0 +1,46 @@
+require 'json'
+require_relative './things'
+
+module JsonReadWrite
+  def json_write(file_name, json_data)
+    existing_data = []
+    existing_data = JSON.parse(File.read(file_name)) if File.exist?(file_name) && !File.empty?(file_name)
+
+    existing_data << json_data
+    File.write(file_name, JSON.pretty_generate(existing_data))
+  end
+
+  def json_read(file_name)
+    case file_name
+    when '../localstorage/book.json'
+      read_books(file_name)
+    when '../localstorage/genre.json'
+      read_genres(file_name)
+    when '../localstorage/music_album.json'
+      read_music_albums(file_name)
+    when '../localstorage/movie.json'
+      read_movies(file_name)
+    when '../localstorage/source.json'
+      read_sources(file_name)
+    when '../localstorage/game.json'
+      read_games(file_name)
+    when '../localstorage/label.json'
+      read_labels(file_name)
+    when '../localstorage/author.json'
+      read_authors(file_name)
+    else
+      []
+    end
+  end
+
+  private
+
+  def read_books(file_name)
+    books = []
+    temp = JSON.parse(File.read(file_name))
+    temp.each do |book|
+      books << Book.new(book['title'], book['author'])
+    end
+    books
+  end
+end
--- modules/things.rb
+++ modules/things.rb
@@ -1,18 +1,32 @@
+require 'json_read_write'
 class Things
+  include JsonReadWrite
   attr_reader :music_albums, :genres, :books, :labels, :authors, :games, :sources
 
   def initialize
-    @music_albums = []
-    @books = []
-    @labels = []
-    @genres = []
-    @authors = []
-    @games = []
-    @sources = []
+    @music_albums = [] || json_read('../localstorage/music_album.json')
+    @books = [] || json_read('../localstorage/book.json')
+    @labels = [] || json_read('../localstorage/label.json')
+    @genres = [] || json_read('../localstorage/genre.json')
+    @authors = [] || json_read('../localstorage/author.json')
+    @games = [] || json_read('../localstorage/game.json')
+    @sources = [] || json_read('../localstorage/source.json')
   end
 
   def add_music_album(music_album)
     @music_albums << music_album unless @music_albums.include?(music_album)
+    json_music_album =
+      {
+        on_spotify: music_album.on_spotify,
+        genre: music_album.genre.name,
+        publish_date: music_album.publish_date,
+        id: music_album.id,
+        label_title: music_album.label.title,
+        author_first_name: music_album.author.first_name,
+        author_last_name: music_album.author.last_name,
+        source: music_album.source.name
+      }
+    json_write('../localstorage/music_album.json', json_music_album)
   end
 
   def add_genre(genre)

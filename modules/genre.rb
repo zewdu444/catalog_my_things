5d87176717b019454186a24fae84ebe8243bbe9b
mohi <mohisadeghi302@gmail.com>
2023-04-25 08:11:01 +0430

    Finish music album

--- modules/genre.rb
+++ modules/genre.rb
@@ -1,7 +1,7 @@
 class Genre
   attr_reader :id, :name, :items
   def initialize(name)
-    @id= rand(1..1000)
+    @id =  Random.rand(1..1000)
     @name = name
     @items = []
   end
--- modules/music_album.rb
+++ modules/music_album.rb
@@ -1,9 +1,11 @@
 require_relative 'item'
 class MusicAlbum < Item
+  attr_accessor :on_spotify
   def initialize(on_spotify)
     @on_spotify = on_spotify
   end
 
   private def can_be_archived?
+    super && @on_spotify
   end
 end

807066622ea0b8a5f6da6aa1abf3c7274fc2ea87
Joseph David <jodavid444dave@gmail.com>
2023-04-25 12:04:38 +0100

    Fix linter errors

--- modules/game.rb
+++ modules/game.rb
@@ -1,13 +1,16 @@
-require  'date'
+require 'date'
 class Game < Item
   attr_reader :multiplayer, :last_played_at
+
   def initialize(publish_date, multiplayer, last_played_at)
     super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
 
-  private def can_be_archived?
+  private
+
+  def can_be_archived?
     if Time.now.year - @last_played_at.year > 2 && super
       true
     else

f7abfce015ccc8d73580f2c6e78e88ed1cfd7c69
mohi <mohisadeghi302@gmail.com>
2023-04-25 09:03:01 +0430

    Fix linters

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -6,7 +6,9 @@ describe MusicAlbum do
   before :each do
     @music_album = MusicAlbum.new(true)
   end
+
   it '@muisic_album should be an instance of MusicAlbum class' do
     expect(@music_album).to be_an_instance_of(MusicAlbum)
   end
+
 end

344a9160242e075feb70e17ab9a026ba02264056
zewdu erkyhun <zewdu444@gmail.com>
2023-04-25 11:55:24 +0300

    Fix linter error

--- modules/book.rb
+++ modules/book.rb
@@ -1,16 +1,18 @@
 class Book < Item
   attr_reader :publisher, :cover_state
+
   def initialize(publish_date, publisher, cover_state)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
 
-  private def can_be_archived?
+  def can_be_archived?
     if @cover_state == 'bad' || super
       true
     else
       false
     end
   end
+  private :can_be_archived?
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,24 +1,24 @@
-require_relative  'spec_helper'
+require_relative 'spec_helper'
 require_relative '../modules/label'
 require_relative '../modules/item'
 require_relative '../modules/book'
 
 
 describe Book do
-    before :each do
-      @book = Book.new('20020-01-01','Amazon', 'good')
-    end
-    it 'takes two parameters and return a book object' do
-      expect(@book).to be_an_instance_of(Book)
-    end
-    it "should be return true if the book is damaged" do
-      @book = Book.new('20011-01-01','Amazon', 'bad')
-      @book.move_to_archive
-      expect(@book.archived).to be true
-    end
-    it "should be return true if the book is older than 10 years" do
-      @book = Book.new('2002-01-01','Amazon', 'good')
-      @book.move_to_archive
-      expect(@book.archived).to be true
-    end
+  before :each do
+    @book = Book.new('20020-01-01', 'Amazon', 'good')
+  end
+  it 'takes two parameters and return a book object' do
+    expect(@book).to be_an_instance_of(Book)
+  end
+  it 'should be return true if the book is damaged' do
+    @book = Book.new('20011-01-01', 'Amazon', 'bad')
+    @book.move_to_archive
+    expect(@book.archived).to be true
+  end
+  it 'should be return true if the book is older than 10 years' do
+    @book = Book.new('2002-01-01', 'Amazon', 'good')
+    @book.move_to_archive
+    expect(@book.archived).to be true
+  end
 end

d440a23ab4228376e3caee95fcb2a35096a35421
zewdu erkyhun <zewdu444@gmail.com>
2023-04-25 17:57:42 +0300

    Modify main and menu file

--- main.rb
+++ main.rb
@@ -1,2 +1,15 @@
 require_relative 'app'
-require_relative 'menu'
+require_relative './app'
+require_relative './modules/things'
+require_relative './menu'
+
+def main
+  things = Things.new
+  app = App.new(things)
+  menu = Menu.new(app)
+  menu.show
+end
+puts 'Hello here! welcome to Catalog of my things App!'
+puts ''
+
+main

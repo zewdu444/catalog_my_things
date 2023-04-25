9fd31e37a6f8ac7d0e12c8dea441fc3a1f95aadf
Mohi <MOHISADEGHI302@GMAIL.com>
2023-04-25 16:40:49 +0330

    Add author property to music album

--- modules/author.rb
+++ modules/author.rb
@@ -1,9 +1,12 @@
 class Author
-  def initialize(first_name, last_name, items)
+  def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    @items = items
+    @items = []
   end
 
-  def add_item(item); end
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.author = self unless item.author == self
+  end
 end

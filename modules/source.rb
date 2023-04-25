84dcafebb56ffe3b7ab78fd04fa55161acb0e4da
Mohi <MOHISADEGHI302@GMAIL.com>
2023-04-25 16:35:46 +0330

    Fix source

--- modules/source.rb
+++ modules/source.rb
@@ -1,8 +1,11 @@
 class Source
-  def initialize(name, _items)
+  def initialize(name)
     @name = name
     @items = []
   end
 
-  def add_item(item); end
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.source = self unless item.source == self
+  end
 end

50e00a3e70d71019298fbf664278aae868528536
Joseph David <jodavid444dave@gmail.com>
2023-04-26 11:11:35 +0100

    Add game table

--- schema.sql
+++ schema.sql
@@ -53,4 +53,14 @@ CREATE TABLE book(
 
 --- game table ---
 
-
+create TABLE game(
+ id serial primary key,
+ multiplayer boolean not null,
+ last_played date not null,
+ publish_date date not null,
+ archived boolean not null,
+ author_id int references author(id),
+ genre_id int references genre(id),
+ source_id int references source(id),
+ label_id int references label(id)
+);
